b730a78d6bf3ae3d2d0277b95998ae1ef64b7c9b
Alexander Travkin <aleverdes.alex@gmail.com>
2024-10-12 19:39:49 +0300

    Editor utility for Archetypes

---
+++ ECS/Archetypes/Editor/ArchetypeCollectionEditorTool.cs
@@ -0,0 +1,20 @@
+﻿#if UNITY_EDITOR
+using GrozaGames.Kit.Archetypes.ScriptableObjects;
+using UnityEditor;
+using UnityEngine;
+
+namespace GrozaGames.Kit.Archetypes.Editor
+{
+    public class ArchetypeCollectionEditorTool
+    {
+        [MenuItem("GrozaGames/Archetypes/Update Archetypes %&l")]
+        public static void FillNewElements()
+        {
+            var archetypeCollection = AssetDatabaseUtils.FindObject<ArchetypeCollection>();
+            archetypeCollection.FillWithNewElements();
+            EditorUtility.SetDirty(archetypeCollection);
+            Debug.Log("Archetypes updated");
+        }
+    }
+}
+#endif
