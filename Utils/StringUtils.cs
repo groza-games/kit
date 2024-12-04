7615cab2886c1a4532dd4237c4f7bb499a97536a
Alexander Travkin <aleverdes.alex@gmail.com>
2024-12-04 20:25:36 +0300

    Fixed ToCamelCase method and added ToPascalCase

--- Utils/StringUtils.cs
+++ Utils/StringUtils.cs
@@ -1,4 +1,6 @@
-using System.Text.RegularExpressions;
+using System;
+using System.Globalization;
+using System.Linq;
 
 namespace GrozaGames.Kit
 {
@@ -6,10 +8,23 @@ namespace GrozaGames.Kit
     {
         public static string ToCamelCase(string str)
         {
-            if (string.IsNullOrEmpty(str))
-                return str;
-            str = Regex.Replace(str, @"(\W+)(\w?)", m => m.Groups[2].Value.ToUpper(), RegexOptions.Compiled);
+            if (string.IsNullOrWhiteSpace(str))
+                return string.Empty;
+
+            str = ToPascalCase(str);
             return char.ToLowerInvariant(str[0]) + str[1..];
+        }
+
+        public static string ToPascalCase(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return string.Empty;
+
+            var textInfo = CultureInfo.InvariantCulture.TextInfo;
+
+            return string.Concat(input
+                .Split(new[] { ' ', '-', '_', '.' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(word => textInfo.ToTitleCase(word.ToLower())));
         }
     }
 }
