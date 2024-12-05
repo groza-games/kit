2430eed6c17ca8c9f0e6204c97266d8681f29aea
Alexander Travkin <aleverdes.alex@gmail.com>
2024-12-05 23:57:16 +0300

    Removed NonLazy command

--- ZenjectExtension/ZenjectExtensions.cs
+++ ZenjectExtension/ZenjectExtensions.cs
@@ -7,47 +7,47 @@ namespace GrozaGames
     {
         public static void BindObjectOnScene<T>(this DiContainer container, T objectOnScene) where T : UnityEngine.Object
         {
-            container.BindInstance(objectOnScene).AsSingle().NonLazy();
+            container.BindInstance(objectOnScene).AsSingle();
         }
         
         public static void BindScenario<T>(this DiContainer container)
         {
-            container.BindInterfacesAndSelfTo<T>().AsSingle().NonLazy();
+            container.BindInterfacesAndSelfTo<T>().AsSingle();
         }
         
         public static void BindService<T>(this DiContainer container)
         {
-            container.BindInterfacesAndSelfTo<T>().AsSingle().NonLazy();
+            container.BindInterfacesAndSelfTo<T>().AsSingle();
         }
         
         public static void BindTask<T>(this DiContainer container)
         {
-            container.BindInterfacesAndSelfTo<T>().AsSingle().NonLazy();
+            container.BindInterfacesAndSelfTo<T>().AsSingle();
         }
         
         public static void BindHandler<T>(this DiContainer container)
         {
-            container.BindInterfacesAndSelfTo<T>().AsSingle().NonLazy();
+            container.BindInterfacesAndSelfTo<T>().AsSingle();
         }
         
         public static void BindScriptableObject<T>(this DiContainer container, T scriptableObject) where T : UnityEngine.ScriptableObject
         {
-            container.BindInstance(scriptableObject).AsSingle().NonLazy();
+            container.BindInstance(scriptableObject).AsSingle();
         }
         
         public static void BindViaSpawnNewPrefab<T>(this DiContainer container, T prefab) where T : UnityEngine.Behaviour
         {
-            container.BindInterfacesAndSelfTo<T>().FromComponentsInNewPrefab(prefab).AsSingle().NonLazy();
+            container.BindInterfacesAndSelfTo<T>().FromComponentsInNewPrefab(prefab).AsSingle();
         }
         
         public static void BindPrefab<T>(this DiContainer container, T prefab) where T : UnityEngine.MonoBehaviour
         {
-            container.BindInstance(prefab).AsSingle().NonLazy();
+            container.BindInstance(prefab).AsSingle();
         }
         
         public static void BindEcsFeature<T>(this DiContainer container) where T : IEcsFeature
         {
-            container.BindInterfacesAndSelfTo<EcsFeatureRegistrator<T>>().AsSingle().NonLazy();
+            container.BindInterfacesAndSelfTo<EcsFeatureRegistrator<T>>().AsSingle();
         }
         
         public static ScreenBindingChain<TScreenView> BindScreenView<TScreenView>(this DiContainer container, TScreenView prefab)
@@ -64,12 +64,12 @@ namespace GrozaGames
             public void BindScreenView(DiContainer container, TScreenView prefab)
             {
                 _container = container;
-                container.BindInstance(prefab).AsSingle().NonLazy();
+                container.BindInstance(prefab).AsSingle();
             }
             
             public void WithScreenController<TScreenController>()
             {
-                _container.BindInterfacesAndSelfTo<TScreenController>().AsSingle().NonLazy();
+                _container.BindInterfacesAndSelfTo<TScreenController>().AsSingle();
             }
         }
     }
