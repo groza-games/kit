83416267df01db235182529eab9da1b53e0c8763
Alexander Travkin <aleverdes.alex@gmail.com>
2025-01-19 14:21:29 +0300

    Additional zenject functionality

--- Localization/LocalizationInstaller.cs
+++ Localization/LocalizationInstaller.cs
@@ -1,21 +1,22 @@
+using System;
 using UnityEngine;
 using Zenject;
 
 namespace GrozaGames.Kit.Localization
 {
+    [RequireComponent(typeof(LocalizationManager))]
     public class LocalizationInstaller : MonoInstaller
     {
         [SerializeField] private LocalizationManager _localizationManager;
-        
+
+        private void Reset()
+        {
+            _localizationManager = GetComponent<LocalizationManager>();
+        }
+
         public override void InstallBindings()
         {
-            Container
-                .BindInterfacesAndSelfTo<LocalizationManager>()
-                .FromComponentInNewPrefab(_localizationManager)
-                .WithGameObjectName("LocalizationManager")
-                .UnderTransformGroup("Infrastructure")
-                .AsSingle()
-                .NonLazy();
+            Container.BindObjectOnScene(_localizationManager);
         }
     }
 }
--- ZenjectExtension/ZenjectAutoSetup.cs
+++ ZenjectExtension/ZenjectAutoSetup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -15,12 +16,48 @@ namespace GrozaGames
             _context = GetComponent<Context>();
         }
         
-        [Button("Set Installers")]
+        [Button("Setup Installers")]
         private void SetInstallers()
         {
             SortAllTransformChildrenByName();
             var installers = GetComponentsInChildren<MonoInstaller>();
             _context.Installers = installers;
+        }
+
+        [Button("Check Project Installers")]
+        private void CheckInstallersInProject()
+        {
+            var contextInstallers = GetComponentsInChildren<MonoInstaller>();
+            var allInstallers = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes().Where(t => t.IsSubclassOf(typeof(MonoInstaller))));
+            foreach (var installer in allInstallers)
+            {
+                if (installer.IsAbstract || installer == typeof(MonoInstaller) || installer == typeof(MonoInstaller<>))
+                    continue;
+
+                if (contextInstallers.All(x => x.GetType() != installer)) 
+                    Debug.LogError($"Installer {installer.Name} not found in context");
+            }
+        }
+
+        [Button("Add All Project Installers")]
+        private void AddInstallersFromProject()
+        {
+            var contextInstallers = GetComponentsInChildren<MonoInstaller>();
+            var allInstallers = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes().Where(t => t.IsSubclassOf(typeof(MonoInstaller))));
+            foreach (var installer in allInstallers)
+            {
+                if (installer.IsAbstract || installer == typeof(MonoInstaller) || installer == typeof(MonoInstaller<>))
+                    continue;
+
+                if (contextInstallers.All(x => x.GetType() != installer))
+                {
+                    var installerGameObject = new GameObject(installer.Name);
+                    installerGameObject.transform.SetParent(transform);
+                    installerGameObject.AddComponent(installer);
+                }
+            }
+            
+            SetInstallers();
         }
         
         private void SortAllTransformChildrenByName()
@@ -28,7 +65,7 @@ namespace GrozaGames
             var list = transform.Cast<Transform>().ToList();
             foreach (var child in list.OrderByDescending(x => x.name))
             {
-                child.gameObject.name = child.gameObject.name.Replace("_Installer", "");
+                child.gameObject.name = child.gameObject.name.Replace("_Installer", "").Replace("Installer", "");
                 child.SetSiblingIndex(0);
             }
         }
